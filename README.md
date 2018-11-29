89532180cda73eecf0c9cffc20dc8b70805cdeda
Ian Walter <public@iankwalter.com>
2018-11-29 16:42:23 -0500

    Improving tests

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # http
 > A transpiled global instance of [ky][kyUrl] (the fetch-based HTTP client)
 
+[![build status][buildImage]][buildUrl]
+
 ## Installation
 
 ```console
@@ -21,10 +23,13 @@ const response = await http.ky.post('/api/thing', { json: { complete: true } })
 
 ## License
 
-Apache 2.0 with Commons Clause - See [LICENSE](https://github.com/ianwalter/http/blob/master/LICENSE)
+Apache 2.0 with Commons Clause - See [LICENSE][licenseUrl]
 
 &nbsp;
 
 Created by [Ian Walter](https://iankwalter.com)
 
 [kyUrl]: https://github.com/sindresorhus/ky
+[buildImage]: https://dev.azure.com/iankw/http/_apis/build/status/ianwalter.http
+[buildUrl]: https://dev.azure.com/iankw/http/_build
+[licenseUrl]: https://github.com/ianwalter/http/blob/master/LICENSE
--- tests/http.test.js
+++ tests/http.test.js
@@ -1,11 +1,33 @@
-const http = require('../')
+import 'whatwg-fetch'
+import http from '../'
+import fetchMock from 'fetch-mock'
 
-test('http has a ky instance', () => {
-  expect(http.ky).toHaveProperty('post')
-})
+describe('Http', () => {
+  it(`can replace it's ky instance`, () => {
+    const headers = { 'content-type': 'application/json' }
+    const newKy = http.ky.extend({ headers })
+    http.replace(newKy)
+    expect(http.ky).toBe(newKy)
+  })
 
-test('http.ky instance can be replaced', () => {
-  const newKy = http.ky.extend({ headers: { 'csrf-token': 'abc123' } })
-  http.replace(newKy)
-  expect(http.ky).toBe(newKy)
+  it('can make a GET request', async () => {
+    try {
+      const msg = { msg: 'Hello World!' }
+      fetchMock.mock('http://myapi.com/msg', msg)
+      expect(await http.ky.get('http://myapi.com/msg').json()).toEqual(msg)
+    } catch (err) {
+      fail(err)
+    }
+  })
+
+  it('throws an HTTPError on a 400 Bad Request response', async () => {
+    try {
+      fetchMock.mock('http://myapi.com/bad', 400)
+      await http.ky.post('http://myapi.com/bad').json()
+      fail('HTTPError was not thrown!')
+    } catch (err) {
+      expect(err.name).toBe('HTTPError')
+      expect(err.message).toBe('Bad Request')
+    }
+  })
 })
