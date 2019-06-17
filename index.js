455abcb9dad833dfa75335f64dda030ad37a271f
Ian Walter <public@iankwalter.com>
2019-06-16 21:03:52 -0400

    Removing dist and adding comments

--- index.js
+++ index.js
@@ -17,30 +17,34 @@ export class Http {
   constructor (options = {}) {
     this.options = options
 
-    // TODO:
+    // Create an insutance method for each HTTP method and just calls the
+    // general fetch method with the HTTP method as the first argument.
     methods.forEach(method => {
       this[method] = async (url, options) => this.fetch(method, url, options)
     })
   }
 
   async fetch (method, url, options = {}) {
-    // TODO:
+    // Create the init options based on some simple defaults, the options set
+    // during class instantiation, and the options passed to the HTTP method.
     const init = { method, headers: {}, ...this.options, ...options }
     init.headers = new window.Headers(init.headers)
 
-    // TODO:
+    // If the given request body is a JavaScript Object, automatically stringify
+    // it and add a JSON content-type header to the request.
     if (typeof init.body === 'object') {
       init.headers.append('Content-Type', 'application/json')
       init.body = JSON.stringify(init.body)
     }
 
-    // TODO:
+    // If there is a base URL set, re-construct the URL to use it.
     if (init.baseUrl) {
       url = init.baseUrl + url
       delete init.baseUrl
     }
 
-    // TODO:
+    // Make the request using the fetch API and construct a custom response
+    // Object.
     const fetchResponse = await window.fetch(url, init)
     const response = {
       ...fetchResponse,
@@ -50,7 +54,8 @@ export class Http {
       statusText: fetchResponse.statusText
     }
 
-    // TODO:
+    // If there is a JSON content-type response header, automatically JSON-parse
+    // the response, otherwise parse it as text.
     const contentType = response.headers.get('Content-Type')
     const isJson = contentType && contentType.indexOf('application/json') > -1
     if (fetchResponse.body && isJson) {
@@ -59,7 +64,8 @@ export class Http {
       response.body = await fetchResponse.text()
     }
 
-    // TODO:
+    // If the response is OK, return the response, otherwise return an HTTPError
+    // instance with the response.
     if (response.ok) {
       return response
     } else {
